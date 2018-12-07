ec757ff0591d3839b19395e9b087c5de6e1935ae
Petrus Rennan <petrus@i9me.com.br>
2018-12-07 17:55:34 -0300

    blank commit

--- template-parts/page/intro-page.php
+++ template-parts/page/intro-page.php
@@ -6,7 +6,7 @@ if(is_home()){
  <section class="Section Section--style1 Section--intro u-displayFlex u-flexDirectionColumn u-flexJustifyContentCenter
  				u-flexAlignItemsCenter u-paddingHorizontal backgroundImage">
 	<header class="Captions u-marginBottom" >
-
+		
 	</header>
 </section>
 
