5318a26f94454b3929815a6e39b736fe6cd78f09
Petrus Rennan <petrus@i9me.com.br>
2019-04-03 16:50:28 -0300

    intro page finalizada estrutura

--- template-parts/header/header-tags.php
+++ template-parts/header/header-tags.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link href="https://fonts.googleapis.com/css?family=Exo+2:200,300,400,700,800" rel="stylesheet"> 
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link href="https://fonts.googleapis.com/css?family=Exo+2:200,300,400,700,800" rel="stylesheet"> 
 
-<?php wp_head(); ?>
+    <!-- FONTE DO PROJETO -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,800" rel="stylesheet">
+
+    <?php wp_head(); ?>
 
 </head>
--- template-parts/page/intro-page.php
+++ template-parts/page/intro-page.php
@@ -3,11 +3,12 @@
 if(is_home()){
 
  ?>
- <section class="Section Section--style1 Section--intro u-displayFlex u-flexDirectionColumn u-flexJustifyContentCenter
- 				u-flexAlignItemsCenter u-paddingHorizontal backgroundImage">
-	<header class="Captions u-marginBottom" >
-		
-	</header>
+ <section class="Section Section--style2 Section--intro u-displayFlex u-flexDirectionColumn u-flexJustifyContentCenter u-flexAlignItemsCenter u-paddingHorizontal backgroundImage">
+	<div class="u-maxSize--container u-alignCenterBox u-paddingVertical u-sizeFull">
+		<header class="Captions u-alignCenter u-size14of24">
+			<h1 class="Captions-title">The spectable before us was indeed sublime.</h1>
+		</header>
+	</div>
 </section>
 
 <?php }else{ ?>
