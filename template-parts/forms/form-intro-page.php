367bedcbc748a147fb7dad8da03abfc376cf1362
Petrus Rennan <petrus@i9me.com.br>
2019-04-03 17:29:51 -0300

    form em desenvolvimento

---
+++ template-parts/forms/form-intro-page.php
@@ -0,0 +1,37 @@
+<form class="Form Form--style1 Form--intro-page u-boxShadow u-borderRadius5 u-paddingHorizontal--inter
+u-paddingVertical--inter u-displayFlex u-flexJustifyContentCenter u-size8of24" action="<?php get_home_url(); ?>">
+
+    <fieldset class="Form-fieldset u-sizeFull">
+        <div class="Form-line u-marginBottom--inter">
+            <div class="Form-row">
+                <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Nome">
+            </div>
+        </div>
+        <div class="Form-line u-marginBottom--inter">
+            <div class="Form-row">
+                <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Sobrenome">
+            </div>
+        </div>
+        <div class="Form-line u-marginBottom--inter">
+            <div class="Form-row">
+                <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="E-mail">
+            </div>
+        </div>
+        <div class="Form-line u-marginBottom--inter">
+            <div class="Form-row">
+                <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Telefone">
+            </div>
+        </div>
+        <div class="Form-line u-marginBottom--inter">
+            <div class="Form-row">
+                <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Empresa">
+            </div>
+        </div>
+        <div class="Form-line u-marginBottom--inter">
+            <div class="Form-row">
+                <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Cargo">
+            </div>
+        </div>
+    </fieldset>
+
+</form>
