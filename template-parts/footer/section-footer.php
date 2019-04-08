698aa2f21d7ad5b390e63932674588f4492e8d79
Petrus Rennan <petrus@i9me.com.br>
2019-04-08 10:34:46 -0300

    estrutura básica pré divisão de soluções para footer

---
+++ template-parts/footer/section-footer.php
@@ -0,0 +1,7 @@
+<section class="Section Section--footer u-sizeFull">
+    <div class="Section-content u-maxSize--container u-alignCenterBox">
+        <div class="Section-header">
+            <h2 class="Section-header-branding"><?php echo get_bloginfo("name"); ?></h2>
+        </div>
+    </div>
+</section>
