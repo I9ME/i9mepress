e2326379e55f85fdb2b8cab5ff1d638cdd5ee2d3
Petrus Rennan <petrus@i9me.com.br>
2019-04-09 16:09:03 -0300

    atualizando estilização do formulário e features

--- template-parts/features/section-features.php
+++ template-parts/features/section-features.php
@@ -1,29 +1,29 @@
 <section class="Section Section--style1 Section--features u-paddingHorizontal">
     <div class="u-maxSize--container u-alignCenterBox u-paddingVertical">
         <ul class="Section-items u-size10of24">
-            <li class="Section-items-item u-displayFlex u-flexAlignItemsCenter u-marginBottom--inter">
-                <div class="Image u-marginRight">
+            <li class="Section-items-item u-displayFlex u-flexAlignItemsCenter u-flexDirectionColumn u-flexSwitchRow u-marginBottom--inter">
+                <div class="Image">
                     <img src="<?php echo get_template_directory_uri() ?>/assets/images/featuretemplate.png" alt="Título da Feature">
                 </div>
-                <div class="Content">
+                <div class="Content u-alignCenter">
                     <h2 class="Section-items-item-title u-paddingBottom--inter--half">Feature 1</h2>
                     <p class="Section-items-item-resume">Aumente a taxa de conversão das suas ações de venda.</p>
                 </div>
             </li>
-            <li class="Section-items-item u-displayFlex u-flexAlignItemsCenter u-marginBottom--inter">
-                <div class="Image u-marginRight">
+            <li class="Section-items-item u-displayFlex u-flexAlignItemsCenter u-flexDirectionColumn u-flexSwitchRow u-marginBottom--inter">
+                <div class="Image">
                     <img src="<?php echo get_template_directory_uri() ?>/assets/images/featuretemplate.png" alt="Título da Feature">
                 </div>
-                <div class="Content">
+                <div class="Content u-alignCenter">
                     <h2 class="Section-items-item-title u-paddingBottom--inter--half">Feature 2</h2>
                     <p class="Section-items-item-resume">Aumente a taxa de conversão das suas ações de venda.</p>
                 </div>
             </li>
-            <li class="Section-items-item u-displayFlex u-flexAlignItemsCenter">
-                <div class="Image u-marginRight">
+            <li class="Section-items-item u-displayFlex u-flexAlignItemsCenter u-flexDirectionColumn u-flexSwitchRow">
+                <div class="Image">
                     <img src="<?php echo get_template_directory_uri() ?>/assets/images/featuretemplate.png" alt="Título da Feature">
                 </div>
-                <div class="Content">
+                <div class="Content u-alignCenter">
                     <h2 class="Section-items-item-title u-paddingBottom--inter--half">Feature 3</h2>
                     <p class="Section-items-item-resume">Aumente a taxa de conversão das suas ações de venda.</p>
                 </div>
--- template-parts/forms/form-intro-page.php
+++ template-parts/forms/form-intro-page.php
@@ -2,42 +2,42 @@
 u-paddingVertical--inter--px u-marginTop--inter u-displayFlex u-flexJustifyContentCenter u-size10of24" action="#">
 
     <fieldset class="Form-fieldset u-sizeFull">
-        <div class="Form-line u-marginBottom--inter">
+        <div class="Form-line u-marginBottom--inter--half">
             <div class="Form-row">
                 <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Nome">
             </div>
         </div>
-        <div class="Form-line u-marginBottom--inter">
+        <div class="Form-line u-marginBottom--inter--half">
             <div class="Form-row">
                 <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Sobrenome">
             </div>
         </div>
-        <div class="Form-line u-marginBottom--inter">
+        <div class="Form-line u-marginBottom--inter--half">
             <div class="Form-row">
                 <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="E-mail">
             </div>
         </div>
-        <div class="Form-line u-marginBottom--inter">
+        <div class="Form-line u-marginBottom--inter--half">
             <div class="Form-row">
                 <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Telefone">
             </div>
         </div>
-        <div class="Form-line u-marginBottom--inter">
+        <div class="Form-line u-marginBottom--inter--half">
             <div class="Form-row">
-                <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Empresa">
+                <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Cidade">
             </div>
         </div>
-        <div class="Form-line u-marginBottom--inter">
+        <div class="Form-line u-marginBottom--inter--half">
             <div class="Form-row">
-                <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Cargo">
+                <input type="text" class="Form-input Form-input--text u-sizeFull" placeholder="Condomínio">
             </div>
         </div>
-        <div class="Form-line u-marginBottom--inter">
+        <div class="Form-line u-marginBottom--inter--half">
             <div class="Form-row">
-                <textarea class="Form-input Form-input--textarea u-sizeFull" placeholder="Conte-nos sobre suas necessidades" rows="10"></textarea>
+                <textarea class="Form-input Form-input--textarea u-sizeFull" placeholder="Conte-nos sobre suas necessidades" rows="5"></textarea>
             </div>
         </div>
-        <div class="Form-line u-marginBottom--inter">
+        <div class="Form-line u-marginBottom--inter--half">
             <div class="Form-row">
                 <input type="submit" class="Button Button--mediumSize Button--background Button--border is-animating u-sizeFull" value="SOLICITAR PROPOSTA">
             </div>
