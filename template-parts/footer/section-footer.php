b1cb6d648b2fdb38904fcd29ebd0a1a61674b649
Petrus Rennan <petrus@i9me.com.br>
2019-04-15 14:40:02 -0300

    endereços dinamizados

--- template-parts/footer/section-footer.php
+++ template-parts/footer/section-footer.php
@@ -2,24 +2,37 @@
 
 $sitename = get_bloginfo("name");
 
+// ENDEREÇOS
+$args = array(
+	"post_type" => "enderecos",
+	"posts_per_page" => 3
+);
+
+$address = new WP_Query($args);
+
 ?>
 
 <section class="Section Section--style2 Section--footer u-paddingHorizontal">
-    <div class="u-maxSize--container u-alignCenterBox u-displayFlex u-flexDirectionColumn u-flexSwitchRow u-flexWrapWrap u-paddingVertical">
-        <div class="Section-content Section-content--branding u-paddingBottom--inter">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/salupp-logo-white.png" alt="<?php echo get_bloginfo("name"); ?>" class="Site-header-branding u-displayBlock u-objectFitCover">
+    <div class="u-maxSize--container u-alignCenterBox u-displayFlex u-flexDirectionColumn u-flexSwitchRow u-flexWrapWrap u-flexJustifyContentSpaceBetween u-paddingVertical">
+        <div class="Section-content u-size6of24 Section-content--branding u-paddingBottom--inter">
+            <a class="<?php echo (is_home() || is_front_page()) ? "u-isScrollDown" : "" ; ?>" href="<?php echo (is_home() || is_front_page()) ? "#page" : get_home_url() ; ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/salupp-logo-white.png" alt="<?php echo get_bloginfo("name"); ?>" class="Site-header-branding u-displayBlock u-objectFitCover">
+            </a>
         </div>
-        <div class="Section-content u-paddingBottom--inter">
-            <header class="Section-header u-paddingBottom--inter--half">
-                <h3 class="Section-header-title">Onde estamos</h3>
-            </header>
-            <div class="Section-content">
-                <p class="Section-content-resume">Av. Lorem Ipsum, 6483</p>
-                <p class="Section-content-resume">Bairro das Flores</p>
-                <p class="Section-content-resume">Cidade (ES)</p>
+            <div class="Section-content u-size6of24 u-paddingBottom--inter">
+                <header class="Section-header u-paddingBottom--inter--half">
+                    <h3 class="Section-header-title">Onde Estamos</h3>
+                </header>
+                <?php if ($address->have_posts()): ?>
+                    <?php while ($address->have_posts()):$address->the_post(); ?>
+                        <div class="Section-content u-marginBottom--inter--half u-displayFlex">
+                            <svg class="u-icon iconLocation u-marginRight"><use xlink:href="#iconLocation"></use></svg>
+                            <p class="Section-content-resume"><?php echo get_the_content(); ?></p>
+                        </div>
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
-        </div>
-        <div class="Section-content u-paddingBottom--inter">
+        <div class="Section-content u-size6of24 u-paddingBottom--inter">
             <header class="Section-header u-paddingBottom--inter--half">
                 <h3 class="Section-header-title">Contato</h3>
             </header>
@@ -34,7 +47,7 @@ $sitename = get_bloginfo("name");
                 </li>
             </ul>
         </div>
-        <div class="Section-content">
+        <div class="Section-content u-size6of24">
             <header class="Section-header u-paddingBottom--inter--half">
                 <h3 class="Section-header-title">Redes Sociais</h3>
             </header>
