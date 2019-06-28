8ca4914eb5b11e0b096b8d31f55d82833938e4c1
Petrus Rennan <petrus@i9me.com.br>
2019-06-28 14:31:40 -0500

    versão 1.0

--- index.php
+++ index.php
@@ -13,12 +13,9 @@
 get_header(); ?>
 <!-- teste -->
 <main id="main" class="SiteMain Site-main" role="main">
-	<?php get_template_part('template-parts/plugins/plugin','lightbox'); ?>
-	<?php get_template_part('template-parts/page/intro','page'); ?>
-	<?php get_template_part('template-parts/diferenciais/section', 'diferenciais'); ?>
-	<?php get_template_part('template-parts/videos/section','videos'); ?>
-	<?php get_template_part('template-parts/features/section','features'); ?>
-	<?php get_template_part('template-parts/depoimentos/section','depoimentos'); ?>
+
+	<!-- ESCREVA AQUI OS CONTROLES DAS SEÇÕES -->
+
 </main><!-- #main -->
 
 
