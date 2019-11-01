2667f9187211883662e41f60bdc21bc24b0439e3
Petrus Rennan <petrus@i9me.com.br>
2019-11-01 09:13:33 -0500

    removendo if e else da home

--- template-parts/header/header-navigation.php
+++ template-parts/header/header-navigation.php
@@ -11,18 +11,10 @@
 	</i>
 </a>
 
-<?php if(is_home()){ ?>
 <nav id="MainNavigation-container" class="Navigation Navigation--menu u-size16of24 Navigation--menu--main Navigation--menu--style2 is-animating u-isCollapsed u-contentHide u-contentHide--height u-zIndex20" role="navigation" aria-label="">
+	
 	<ul class="Navigation-items Navigation-items--level1 u-alignCenter u-marginTop--inter is-animating u-displayFlex u-sizeFull u-flexDirectionColumn u-flexSwitchRow u-justifyContentSpaceBetween">
 		
 	</ul>
 
 </nav><!-- #site-navigation -->
-<?php }else{ ?>
-	<nav id="MainNavigation-container" class="Navigation Navigation--menu u-size18of24  Navigation--menu--main Navigation--menu--style2 is-animating u-isCollapsed u-contentHide u-contentHide--height u-zIndex20" role="navigation" aria-label="">
-	<ul class="Navigation-items Navigation-items--level1 u-alignCenter u-marginTop--inter is-animating u-displayFlex u-sizeFull u-flexDirectionColumn u-flexSwitchRow u-justifyContentSpaceBetween">
-		<!-- HEADER LIMPO  -->
-	</ul>
-
-</nav><!-- #site-navigation -->
-<?php } ?>
